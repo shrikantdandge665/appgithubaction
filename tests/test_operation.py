9b30b52b81e47613755795bd107f956cfdec5e7d
shrikantdandge665 <shrikantdandge665@gmail.com>
2024-10-19 16:28:16 +0530

    Unity test updated

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_opertation import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(1,-1)==0
+
+def test_sub():
+    assert sub(5,2)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+
